bb0129e76bec360d6e257337741f7b21d2a89867
michaelvcolianna <michael.colianna@me.com>
2022-12-13 22:19:57 -0500

    Adds the accent image component and styles

--- storyblok/AccentImage.vue
+++ storyblok/AccentImage.vue
@@ -3,7 +3,28 @@ const props = defineProps({ blok: Object })
 </script>
 
 <template>
-  <div v-editable="blok" class="clipped">
-    <pre>ACCENT IMAGE {{ blok }}</pre>
-  </div>
+  <section v-editable="blok" aria-hidden="true" class="clipped">
+    <StoryblokImage
+      :decorative="true"
+      :image="blok.image"
+      height="500"
+      width="340"
+      sizes="xs:100vw sm:100vw md:100vw lg:100vw xl:100vw"
+    />
+  </section>
 </template>
+
+<style scoped lang="scss">
+section {
+  background-color: var(--color-white);
+  padding: 1rem;
+}
+
+img {
+  display: block;
+  height: var(--accent-height);
+  object-fit: cover;
+  object-position: var(--accent-position);
+  width: 100%;
+}
+</style>
