dba8708b15e7f739a154740a5a7afa983ab4f43b
michaelvcolianna <michael.colianna@me.com>
2023-01-01 21:47:13 -0500

    Adds super basic styling to 404 page

--- error.vue
+++ error.vue
@@ -3,8 +3,29 @@ const props = defineProps({ error: Object })
 </script>
 
 <template>
-  <div>
-    <h1>error.vue</h1>
-    <pre>{{ error }}</pre>
-  </div>
+  <NuxtLayout>
+    <section id="error">
+      <div class="inner" role="alert">
+        <h1>Oops! Something went wrong!</h1>
+
+        <h2>Error {{ error.statusCode }}</h2>
+
+        <p>{{ error.statusMessage }}</p>
+      </div>
+    </section>
+  </NuxtLayout>
 </template>
+
+<style scoped lang="scss">
+#error {
+  background-color: var(--color-grey-light);
+}
+
+.inner {
+  display: grid;
+  gap: 2rem;
+  margin: auto;
+  max-width: 80vw;
+  padding: 4rem 0;
+}
+</style>
